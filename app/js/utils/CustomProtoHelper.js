420f9606bc5afc57b73a4fb0ddbfbb2e96621f8f
unreg <black@tmnhy.su>
2018-01-12 17:05:54 +0300

    Add settings

--- app/js/utils/CustomProtoHelper.js
+++ app/js/utils/CustomProtoHelper.js
@@ -1,5 +1,8 @@
 'use strict';
 
+const { path } = require('./settings');
+
+
 var isiOS = navigator.userAgent.match('iPad') || navigator.userAgent.match('iPhone') || navigator.userAgent.match('iPod');
 var isAndroid = navigator.userAgent.match('Android');
 var isMobile = isiOS || isAndroid;
@@ -7,7 +10,7 @@ var isMobile = isiOS || isAndroid;
 var match = document.location.pathname.match(/\/join\/(.+)/);
 if (match) var token = match[1];
 
-var joinLink = 'https://app.actor.im/#/join/' + token;
+var joinLink = path.joinLink + '/#/join/' + token;
 var customProtocolLink = 'actor://invite?token=' + token;
 
 module.exports = {
---
+++ app/js/utils/settings.js
@@ -0,0 +1,16 @@
+
+const appName = '';
+
+const path = {
+  serverDomain: '', // server domain, e.g. 'actor.im'
+  androidDownload: 'https://', // android app download, e.g. 'https://actor.im/android'
+  iosDownload: 'https://', // ios app download, e.g. 'https://actor.im/ios'
+  joinLink: 'https://', // e.g. https://app.actor.im'
+  apiLink: 'https://', // same as base-url in server.conf,  e.g. 'https://api.actor.im'
+  downloadLink: 'https://', // e.g. 'https://actor.im/dl
+}
+
+module.exports = {
+    appName: appName,
+    path: path
+};
